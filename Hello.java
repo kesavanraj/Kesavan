ec2c20f557216c202177e41254ef1b8c714ded84
kesavanraj <kesavanraj28@gmail.com>
2017-06-30 17:59:50 +0530

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+public class Hello
+{
+public static void main(String args[])throws IOException
+{
+BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+String s;
+for(s=0;s<=5;s++)
+{
+System.out.println("Hello");
+}
+}
+}
