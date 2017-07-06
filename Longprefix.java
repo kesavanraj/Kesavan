3084a260f6b5773702b41e5116578b882fb8c3d9
kesavanraj <kesavanraj28@gmail.com>
2017-07-06 11:19:59 +0530

    Update Longprefix.java

--- Longprefix.java
+++ Longprefix.java
@@ -1,31 +1,50 @@
 import java.io.*;
 import java.util.*;
 import java.lang.*;
-public class Longprefix
-{
-public static void main(String ar[])throws Exception
-{
-Scanner s=new Scanner(System.in);
-int b,c;
-b=sc.nextInt();
-String a[]=new String[100];
-for(int i=0;i<b;i++)
-{
-a[i]=sc.next();
-}
-String long,check;
-if(a.length>0)
-long=a[0];
-for(int i=1;i<a.length;i++)
-{
-check=a[i];
-for(c=0;c<Math.min(long.length(),a[i].length());c++)
-{
-if(long.charAt(c)!=check.charAt(c))
-break;
-}
-long=a[i].subString(0,c);
-}
-System.out.println("longest prefix is:"+long);
-}
+
+
+public class Longprefix {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader bb = new BufferedReader(new InputStreamReader(System.in));
+		Integer n,i,min=0,j,count,max=0;
+		ArrayList<String> al = new ArrayList<String>();
+		
+		n = Integer.parseInt(bb.readLine());
+		String[] s = new String[n];
+		Integer[] ii = new Integer[n];
+		String[] s1 = new String[n];
+		for(i=0;i<s.length;i++){
+			s[i] = bb.readLine();
+			al.add(s[i]);
+		}
+		
+		Collections.sort(al);
+		
+		ii[0] = 0;
+		s1[0] = "";
+		for(i=0;i<al.size()-1;i++){
+			count = 0;
+			min = Math.min(al.get(i).length() , al.get(i+1).length());
+			for(j=0;j<min;j++){
+				if(al.get(i).charAt(j) == al.get(i+1).charAt(j))
+					count++;
+				else
+					break;
+			}
+			ii[i+1] = count;
+			s1[i+1] = al.get(i).substring(0,count);
+			//System.out.println(count +","+s1[i+1]);
+		}
+		
+		for(i=0;i<ii.length-1;i++)
+			max = Math.max(max,ii[i+1]);
+		//System.out.println("max = "+max);
+		
+		for(i=0;i<s1.length;i++){
+			if(max == ii[i])
+				System.out.println(s1[i]);
+		}
+	}
+
 }
